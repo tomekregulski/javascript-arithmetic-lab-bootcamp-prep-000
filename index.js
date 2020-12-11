c39667d8d2fb18fb1e2b2578d710afe7dcecca75
Thomas Regulski <tomek.regulski@gmail.com>
2020-12-11 00:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,24 +8,6 @@ function divideBy3() {
   number /= 3
 }
 
-divideBy3() 
-
-console.log(number)
-
-add5()
-
-console.log(number)
-
-number = 10
-
-add5()
-
-console.log(number)
-
-divideBy3()
-
-console.log(number)
-
 function increments() {
   number++
 }
